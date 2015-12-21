a2b8ddad3f9b6b88795dfdb8cd25cc0c6b270f96
宿莽 <842766898@qq.com>
2015-12-21 11:51:45 +0800

    在一个 view上用数组添加多个控件
    设置两个数组作为属性，懒加载；用数组间隔的添加控件

--- Weibo/Classes/Home/View/StatusTooBbar.m
+++ Weibo/Classes/Home/View/StatusTooBbar.m
@@ -7,8 +7,25 @@
 //
 
 #import "StatusTooBbar.h"
-
+@interface StatusTooBbar()
+@property(nonatomic,strong)NSMutableArray *btnCounts;
+@property(nonatomic,strong)NSMutableArray *dividerCounts;
+@end
 @implementation StatusTooBbar
+
+-(NSMutableArray *)btnCounts{
+    if (_btnCounts == nil) {
+        _btnCounts = [NSMutableArray array];
+    }
+    return _btnCounts;
+}
+
+-(NSMutableArray *)dividerCounts{
+    if (_dividerCounts == nil) {
+        _dividerCounts = [NSMutableArray array];
+    }
+    return _dividerCounts;
+}
 
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
@@ -24,9 +41,17 @@
         [self setupBtnWithTitle:@"赞" image:@"timeline_icon_unlike" bgImage:@"timeline_card_rightbottom_highlighted"];
         
         //设置分割图片
-        
+        [self setupDivider];
+        [self setupDivider];
     }
     return self;
+}
+
+-(void)setupDivider{
+    UIImageView *divider = [[UIImageView alloc]init];
+    divider.image = [UIImage imageWithName:@"timeline_card_bottom_line"];
+    [self addSubview:divider];
+    [self.dividerCounts addObject:divider];
 }
 
 -(void)setupBtnWithTitle:(NSString *)title image:(NSString *)image bgImage:(NSString *)bgImage{
@@ -39,18 +64,31 @@
     [btn setImage:[UIImage imageWithName:image] forState:UIControlStateNormal];
     [btn setBackgroundImage:[UIImage resizeImageWithName:bgImage] forState:UIControlStateHighlighted];
     [self addSubview:btn];
+    [self.btnCounts addObject:btn];
 }
 
 -(void)layoutSubviews{
     [super layoutSubviews];
-    CGFloat btnW = self.frame.size.width / self.subviews.count;
+    CGFloat divW = 2;
+    CGFloat btnW = (self.frame.size.width - self.dividerCounts.count*divW)/ self.btnCounts.count;
     CGFloat btnH = self.frame.size.height;
     //CGFloat btnY = self.frame.origin.y;
     CGFloat btnY = 0;
-    for (int i = 0; i<self.subviews.count; i++) {
-        UIButton *btn = self.subviews[i];
-        CGFloat btnX = i*btnW;
+    for (int i = 0; i<self.btnCounts.count; i++) {
+        UIButton *btn = self.btnCounts[i];
+        CGFloat btnX = i*(btnW +divW);
         btn.frame = CGRectMake(btnX, btnY, btnW, btnH);
+    }
+    CGFloat divH = btnH;
+   
+    CGFloat divY = 0;
+    for (int j = 0; j<self.dividerCounts.count; j++) {
+        UIButton *btn = self.btnCounts[j];
+        UIImageView *divider = self.dividerCounts[j];
+        //CGFloat divX = (j + 1)*btnW;
+        CGFloat divX = CGRectGetMaxX(btn.frame);
+        divider.frame = CGRectMake(divX, divY, divW, divH);
     }
 }
 @end
+
